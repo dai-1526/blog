f854cd127782c826ea6b68369de09f5629dc95d7
dai-1526 <dai1525@icloud.com>
2024-03-21 11:36:42 +0900

    編集画面作成　更新後処理

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,14 +9,14 @@
         <form action = "/posts" method = "POST">
             @csrf
             <div class = "title">
-                <h2>Title</h2>
+                <h2>タイトル</h2>
                 <input type = "text" name = "post[title]" placeholder = "タイトル" value = "{{old('post.title') }}"/>
                 <p class = "title_error" style = "color:red">
                     {{ $errors ->first('post.title') }}
                 </p>
             </div>
             <div class = "body">
-                <h2>body</h2>
+                <h2>本文</h2>
                 <textarea name = "post[body]" placeholder = "お疲れ様でした。">{{ old('post.body') }}</textarea>
                 <p class = "body_error" style = "color:red">
                     {{ $errors ->first('post.body') }}
